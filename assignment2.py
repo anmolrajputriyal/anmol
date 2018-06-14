7f76bed525c47978bba6e03a9865bd0ce106fa5f
anmolrajputriyal <40023066+anmolrajputriyal@users.noreply.github.com>
2018-06-14 23:28:38 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,25 @@
+#Q1
+print("hello this is my output")
+
+#Q2
+print("acad"+"view")
+
+#Q3
+a=2
+b=4
+c=7
+print(a,b,c)
+
+#Q4
+print('let\'s get started')
+
+#Q5
+s="acadview"
+course="python"
+fees=5000
+print("hello welcome to %s your course name is %s and total amount pending rs %d"%(s,course,fees))
+
+#Q6
+name="tony"
+salary=200000
+print('%s''%d'%(name,salary))
