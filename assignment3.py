0a2493428fc17d2338fbbbe8b89445e138d24491
anmolrajputriyal <40023066+anmolrajputriyal@users.noreply.github.com>
2018-06-14 23:40:29 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,31 @@
+#Q1
+l=[]
+a=input("enter number")
+l.append(a)
+print(l)
+
+#Q2
+a=input("enter the string")
+l.append(a)
+print(l)
+
+#Q3
+emotions=["love","happy","hate","single","happy","sad","happy"]
+print(emotions.count("happy"))
+
+#Q4
+a=[1,2,4,5,7]
+b=[6,7,5,4,8]
+a.sort()
+print(a)
+b.sort()
+l=(a+b)
+l.sort()
+print(l)
+
+#Q5
+l=[1,2,3,4,5]
+(l.append(6))
+print(1)
+l.pop(2)
+print(l)
