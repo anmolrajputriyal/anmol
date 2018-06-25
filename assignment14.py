a50f4e390f3ea195dc14a806408f413d85206d13
anmolrajputriyal <40023066+anmolrajputriyal@users.noreply.github.com>
2018-06-25 23:38:25 +0530

    Add files via upload

---
+++ assignment14.py
@@ -0,0 +1,22 @@
+import re
+#Q1
+email=["zuck26@facebook.com","page23@google.com","jeff42@amazon.com"]
+ls=[]
+for i in email:
+    a=re.findall("[\w]+",i)
+    a=tuple(a)
+    ls.append(a)
+print(ls)
+
+#Q2
+text = "Betty bought a bit of butter, But the butter was so bitter, So she bought some better butter, To make the bitter butter better."
+name=re.findall("[b,B][a-z][A-Z]{1,5}",text)
+print(name)
+
+#Q3
+sentence = "A, very very; irregular_sentence"
+print(sentence)
+a=re.compile("[,;_]")
+for i in sentence:
+    b=a.sub(" ",sentence)
+print(b)
